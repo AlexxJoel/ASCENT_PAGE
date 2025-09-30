9db4d7481e187c702896c623d6dbeaa78d817d56
AlexxJoel <20213tn019@utez.edu.com>
2025-09-30 16:45:58 -0600

    Added custom Tailwind plugin for `.prose` styles and updated services layout.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,25 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss/plugin')(({ addBase, theme }) => {
+			addBase({
+				'.prose': {
+					color: theme('colors.gray.300'),
+				},
+				'.prose h2': {
+					color: theme('colors.white'),
+					borderBottom: `2px solid ${theme('colors.blue.600')}`,
+					paddingBottom: '0.5rem',
+					marginTop: '2rem',
+					marginBottom: '1rem',
+				},
+				'.prose ul': {
+					listStyleType: 'disc',
+					paddingLeft: '1.5rem',
+					color: theme('colors.gray.300'),
+				},
+			});
+		}),
+	],
 }
